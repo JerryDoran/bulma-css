99b46bee4656e958f18f858082540c8603ee9547
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-10-07 12:11:57 -0400

    Added tabbed content and message box

--- bulma_ninja_project/index.js
+++ bulma_ninja_project/index.js
@@ -5,3 +5,23 @@ const navLinks = document.getElementById('navLinks');
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('is-active');
 });
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tabContent > div');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((item) => item.classList.remove('is-active'));
+    tab.classList.add('is-active');
+
+    const target = tab.dataset.target;
+    tabContentBoxes.forEach((box) => {
+      if (box.getAttribute('id') === target) {
+        box.classList.remove('is-hidden');
+      } else {
+        box.classList.add('is-hidden');
+      }
+    });
+  });
+});
